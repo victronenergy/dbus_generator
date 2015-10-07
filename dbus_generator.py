03567bce19ad7123fb88fceeec29752872f2743b
Jesús Miguel Pérez Fernandez <jperez@victronenergy.com>
2015-10-07 14:50:52 +0200

    Change DBus settings paths

--- dbus_generator.py
+++ dbus_generator.py
@@ -32,7 +32,7 @@ from dbusmonitor import DbusMonitor
 from settingsdevice import SettingsDevice
 from logger import setup_logging
 
-softwareversion = '1.2'
+softwareversion = '1.2.1'
 dbusgenerator = None
 
 
