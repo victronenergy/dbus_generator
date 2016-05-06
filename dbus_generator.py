f08ad11754be3cda298499d1bffcfe4e7c279d51
Jesús Miguel Pérez Fernandez <jperez@victronenergy.com>
2016-05-06 12:45:16 +0200

    version bumped to 1.2.8

--- dbus_generator.py
+++ dbus_generator.py
@@ -32,7 +32,7 @@ from dbusmonitor import DbusMonitor
 from settingsdevice import SettingsDevice
 from logger import setup_logging
 
-softwareversion = '1.2.7'
+softwareversion = '1.2.8'
 dbusgenerator = None
 
 
