9cb35170b2a676fc4691024533571efe0a71079b
Jesus Perez <jperez@victronenergy.com>
2016-12-08 12:47:39 +0100

    Version bumped to v1.3.2

--- dbus_generator.py
+++ dbus_generator.py
@@ -31,7 +31,7 @@ from dbusmonitor import DbusMonitor
 from settingsdevice import SettingsDevice
 from logger import setup_logging
 
-softwareversion = '1.3.1'
+softwareversion = '1.3.2'
 dbusgenerator = None
 
 
