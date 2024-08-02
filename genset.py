850b2ffd63e85b5b146354b978c2b6e0491e3972
ReinvdZee <rvanderzee@victronenergy.com>
2024-08-02 10:23:32 +0200

    Helper relay for connected gensets
    Inteded for connected gensets which cannot be started and stopped
    over Modbus. These gensets do not report a valid '/Start' path.
    If this is the case, a helper relay must be configured which start
    and stops the genset.
    
    For connected gensets which do support start and stop commands over
    Modbus, the helper relay can still be configured and will be enabled/disabled
    in conjunction with the genset (which is controlled with '/Start').
    
    The option 5:"Connected genset helper relay" is added to the gui for this.
    
    https://github.com/victronenergy/venus/issues/1286

--- genset.py
+++ genset.py
@@ -26,6 +26,14 @@ monitoring = {
 		'/ProductId': dummy,
 		'/DeviceInstance': dummy,
 		'/Start': dummy
+		},
+	'com.victronenergy.settings': {
+		'/Settings/Relay/Function': dummy,
+		'/Settings/Relay/Polarity': dummy,
+		},
+	'com.victronenergy.system': {
+		'/Relay/0/State': dummy,
+		'/DeviceInstance': dummy,
 		}
 	}
 
@@ -45,8 +53,11 @@ def create(dbusmonitor, remoteservice, settings):
 
 class Genset(StartStop):
 	_driver = 1 # Genset service
+	_helperrelayservice = None
+
 	def _remote_setup(self):
 		self.enable()
+		self._check_enable_conditions(self._dbusmonitor.get_value('com.victronenergy.settings', '/Settings/Relay/Function'))
 
 	def _check_remote_status(self):
 		error = self._dbusservice['/Error']
@@ -62,12 +73,47 @@ class Genset(StartStop):
 		elif remotestart == 1 and error == Errors.REMOTEDISABLED:
 			self.clear_error()
 
+	def _check_enable_conditions(self, relaysetting):
+		# If there's a helper relay, the start/stop service is enabled.
+		if (relaysetting == 5):
+			self._helperrelayservice = 'com.victronenergy.system'
+			self._dbusservice['/Enabled'] = 1
+			# Set relay state.
+			super()._update_remote_switch()
+		# If there's no helper relay but the genset has '/Start', the start/stop service is also enabled.
+		elif self._dbusmonitor.seen(self._remoteservice, '/Start'):
+			self._dbusservice['/Enabled'] = 1
+			self._helperrelayservice = None
+		# If the genset does not have '/Start' and there is also no helper relay, disable the start/stop service.
+		else:
+			self._dbusservice['/Enabled'] = 0
+			self._set_remote_switch_state(0)
+			self._helperrelayservice = None
+
+	def dbus_value_changed(self, dbusServiceName, dbusPath, options, changes, deviceInstance):
+		if self._dbusservice is None:
+			return
+
+		if "/Settings/Relay/Function" in dbusPath:
+			self._check_enable_conditions(changes['Value'])
+
+		super().dbus_value_changed(dbusServiceName, dbusPath, options, changes, deviceInstance)
+
+		# Make sure that the relay polarity is set to normally open.
+		if self._helperrelayservice is not None and self._dbusmonitor.get_value('com.victronenergy.settings', '/Settings/Relay/Polarity') == 1:
+			self._dbusmonitor.set_value('com.victronenergy.settings', '/Settings/Relay/Polarity', 0)
+
 	def _get_remote_switch_state(self):
 		# Do not drive the remote switch in case of error
 		# because Fischer Panda genset will clear the error when switched off
 		if self.get_error() in [Errors.REMOTEDISABLED, Errors.REMOTEINFAULT]:
 			return 0
-		return self._dbusmonitor.get_value(self._remoteservice, '/Start')
+		if (not self._dbusservice['/Enabled']):
+			return 0
+		if self._helperrelayservice is not None:
+			return self._dbusmonitor.get_value(self._helperrelayservice, '/Relay/0/State')
+		else:
+			return self._dbusmonitor.get_value(self._remoteservice, '/Start')
 
 	def _set_remote_switch_state(self, value):
 		error = self._dbusservice['/Error']
@@ -76,3 +122,6 @@ class Genset(StartStop):
 		if error in [Errors.REMOTEDISABLED, Errors.REMOTEINFAULT]:
 			return
 		self._dbusmonitor.set_value_async(self._remoteservice, '/Start', value)
+
+		if (self._helperrelayservice):
+			self._dbusmonitor.set_value_async(self._helperrelayservice, '/Relay/0/State', value)
--- startstop.py
+++ startstop.py
@@ -317,6 +317,7 @@ class StartStop(object):
 		self._dbusservice.add_path('/Type', value=self._driver)
 		# State: None = invalid, 0 = stopped, 1 = running, 2=Warm-up, 3=Cool-down
 		self._dbusservice.add_path('/State', value=None, gettextcallback=lambda p, v: States.get_description(v))
+		self._dbusservice.add_path('/Enabled', value=1)
 		# RunningByConditionCode: Numeric Companion to /RunningByCondition below, but
 		# also encompassing a Stopped state.
 		self._dbusservice.add_path('/RunningByConditionCode', value=None)
