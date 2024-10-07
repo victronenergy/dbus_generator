2cfd88757d3608f025eae5f9e9987195c66e4406
ReinvdZee <rvanderzee@victronenergy.com>
2024-10-07 15:35:30 +0200

    Make sure startstop reaches 'STOPPED' state
    Since the runtime counting improvements (fe7beb3af842ddb74aec067569cb87ad88232590), runtime can be reset externally.
    So make to sure to not rely on the `/Runtime` field when evaluating
    the cooldown time and generator stoptime.
    
    https://github.com/victronenergy/venus/issues/1360

--- startstop.py
+++ startstop.py
@@ -635,8 +635,12 @@ class StartStop(object):
 
 		if start:
 			self._start_generator(startbycondition)
-		elif (self._dbusservice['/Runtime'] >= self._settings['minimumruntime'] * 60
-			  or activecondition == 'manual'):
+		# Do not fully rely on the '/Runtime', since that can be reset externally when e.g. the digital input is off.
+		# So also call _stop_generator if the generator is already stopping (COOLDOWN or STOPPING).
+		# This allows the runtime to be reset before startstop has reached "STOPPED"
+		elif (self._dbusservice['/State'] in (States.COOLDOWN, States.STOPPING)
+				or self._dbusservice['/Runtime'] >= self._settings['minimumruntime'] * 60
+				or activecondition == 'manual'):
 			self._stop_generator()
 
 	def _update_runtime(self, just_stopped=False):
